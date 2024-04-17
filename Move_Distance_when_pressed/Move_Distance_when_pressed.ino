b9844cefecf03e78a831ffd9c333ee9915f3e326
Quincy Owyang <102776831+0uincy@users.noreply.github.com>
2024-04-16 22:24:13 -0700

    Update Move_Distance_when_pressed.ino

--- Move_Distance_when_pressed/Move_Distance_when_pressed.ino
+++ Move_Distance_when_pressed/Move_Distance_when_pressed.ino
@@ -3,13 +3,13 @@
 
 #include <AccelStepper.h>
 
-const int goPin = 36;  //activation button pinout
+const int goPin = 13;  //activation button pinout
 
 //Define motors & their respective direction and step pins
-AccelStepper stepper1( AccelStepper::DRIVER, 14 , 12 );
-AccelStepper stepper2( AccelStepper::DRIVER, 32 , 33 );
+AccelStepper stepper1( AccelStepper::DRIVER, 33 , 25 );    //step, dir
+AccelStepper stepper2( AccelStepper::DRIVER, 27 , 14 );
 
-float acceleration = 0.035; //in m/s^2
+float acceleration = 35;    //in mm/s^2
 float maxSpeed = 250;       //in mm/s
 int travelDistance = 150;   //in mm
 int conversion = 160;       //in steps/mm
@@ -24,18 +24,18 @@ int stop;
 void setup()
 { 
   //Convert distance & acceleration to steps
-  acc = acceleration*1000*conversion;
+  acc = acceleration*conversion;
   speed = maxSpeed*conversion;
   travel = travelDistance*conversion;
 
-  go = 0;    //value of 1 to initiate motor movement
-  state = 0; //0 for down position, 1/HIGH for up position
+  go = 0;     //value of 1 to initiate motor movement
+  state = 0;  //0 = bottom, 1 = top
 
   //Initialize pins & invert enable pin
   pinMode(goPin,INPUT);
-  stepper1.setEnablePin(13);
-  stepper2.setEnablePin(25); 
-  stepper1.setPinsInverted(false, false, true); //direction, step, enable
+  stepper1.setEnablePin(32);
+  stepper2.setEnablePin(26); 
+  stepper1.setPinsInverted(true, false, true); //direction, step, enable
   stepper2.setPinsInverted(false, false, true);
 
   //Define motor parameters
@@ -43,8 +43,9 @@ void setup()
   stepper2.setMaxSpeed(speed);
   stepper1.setAcceleration(acc);
   stepper2.setAcceleration(acc);
-  stepper1.setCurrentPosition(travel);
-  stepper2.setCurrentPosition(travel);
+
+  stepper1.setCurrentPosition(0);
+  stepper2.setCurrentPosition(0);
 }
  
 void loop() //Movement control
@@ -52,14 +53,31 @@ void loop() //Movement control
   go = digitalRead(goPin); //read control pin
 
   if (go == HIGH){
-    //Go down and disable motors
-    if (state == 0){
+    //Go up
+    if (state == 0){ 
       stepper1.enableOutputs();
       stepper2.enableOutputs();
-
+      
       stepper1.moveTo(travel);
       stepper2.moveTo(travel);
-      while (stepper1.distanceToGo()!= 0){
+
+      while (stepper1.distanceToGo() != 0){
+        stepper1.run();
+        stepper2.run();
+      }
+
+      state = 1;
+    }
+
+    //Go down and disable motors
+    else if (state == 1){
+      stepper1.enableOutputs();
+      stepper2.enableOutputs();
+      
+      stepper1.moveTo(0);
+      stepper2.moveTo(0);
+
+      while (stepper1.distanceToGo() != 0){
         stepper1.run();
         stepper2.run();
       }
@@ -67,24 +85,9 @@ void loop() //Movement control
       stepper1.disableOutputs();
       stepper2.disableOutputs();
 
-      state = 1;
-    }
-
-    //Go up
-    else if (state == 1){ 
-      stepper1.enableOutputs();
-      stepper2.enableOutputs();
-      
-      stepper1.moveTo(0);
-      stepper2.moveTo(0);
-      while (stepper1.distanceToGo()!= 0){
-        stepper1.run();
-        stepper2.run();
-      }
-
       state = 0;
     }
   }
 
-  delay(1);
+  delay(10);
 }
